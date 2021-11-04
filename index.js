fbda7d0ded6cec6d5e7682a97865ec6295ab32f5
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-11-03 19:53:54 -0600

    commit tree dbc850a3ca73f77fc5e6ecb4b802cd62d749184d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+
+
+const returnFirstTwoDrivers = function (drivers) {
+    return [drivers[0], drivers[1]];
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+function createFareMultiplier(integer) {
+    return function multiplier(fare) {
+        return fare * integer;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driverPair) {
+    return driverPair;
+}
+
+selectDifferentDrivers(drivers, returnFirstTwoDrivers)
